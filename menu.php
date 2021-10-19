3f4578216515e858a6d779d7bdc25707213ed8d2
Jenifer19IFC <jenifergoedert10@gmail.com>
2021-10-18 21:48:46 -0300

    erro de digitação

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h2>JOGOS:</h2>
-<h3><a href="index.php">Jodo do Par ou Ímpar</a></h3>
+<h3><a href="index.php">Jogo do Par ou Ímpar</a></h3>
 <h3><a href="index1.php">Jogo de Dados</a></h3>
 <h3><a href="index2.php">Jogo Pedra, Papel, Tesoura</a></h3>
 </body>
